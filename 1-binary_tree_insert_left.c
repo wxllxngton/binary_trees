e781357946f3b86623d2b49f9b93535ba51f8f93
Shannon-Kioko <shannonkaluki@gmail.com>
2023-11-02 11:38:03 -0700

    Add some fixes according ti Betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,29 +13,29 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return NULL;
+		if (parent == NULL)
+				return NULL;
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return NULL;
+		binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+		if (new_node == NULL)
+				return NULL;
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->right = NULL;
+		new_node->n = value;
+		new_node->parent = parent;
+		new_node->right = NULL;
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
-    else
-    {
-        new_node->left = NULL;
-    }
+		if (parent->left != NULL)
+		{
+				new_node->left = parent->left;
+				parent->left->parent = new_node;
+		}
+		else
+		{
+				new_node->left = NULL;
+		}
 
-    parent->left = new_node;
+		parent->left = new_node;
 
-    return new_node;
+		return new_node;
 }
 
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,8 +9,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+		if (node == NULL)
+				return (0);
 
-    return (node->parent == NULL);
+		return (node->parent == NULL);
 }
